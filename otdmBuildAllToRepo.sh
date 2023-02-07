51b66100b3acdad11b9171d90cd9f372d7a5f798
yoyo <yoyo@yoyody>
2023-02-07 10:49:50 -0500

    first set of .debs :)

---
+++ otdmBuildAllToRepo.sh
@@ -0,0 +1,36 @@
+bList="otdm-cmqtt2mysql \
+otdm-db-init \
+otdm-db-init-dummy \
+otdm-grafana-ds-MySql \
+otdm-installer-dummy \
+otdm-intranetrepo-debianish \
+otdm-intranetrepo-termux \
+otdm-mqtt-installer \
+otdm-mqtt-installer-dummy \
+otdm-mrepo \
+otdm-node-red-installer \
+otdm-nrf-yss \
+otdm-tools"
+
+echo "build all repository elements....."
+
+eList=""
+
+for e in ${bList}
+do
+  echo "-------------------------------------"
+  echo "making now ["${e}"]"
+  otdmMake.sh ${e}
+  r=$?
+  if [ ${r} = "0" ]
+  then
+    echo "finish Correctly-------------------"
+  else
+    echo "Error at end ----------------------"
+    eList=${eList}", "${e}
+  fi
+
+
+done
+
+echo "Errors in total in: ["${eList}"]"
