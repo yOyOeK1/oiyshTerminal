ece4bd5cb2dda7af3bacd83626c5e7c869b8a7ab
Bartlomiej Ceglik <yoyo@yoyody>
2023-02-15 08:04:06 -0500

    repo update

--- otdm2jsdocs.sh
+++ otdm2jsdocs.sh
@@ -10,14 +10,30 @@ wikiDir="/home/yoyo/Apps/oiyshTerminal.wiki"
 #
 
 list='{
-  "mMath": {"title": "mMath and string helper"},
-  "mOperation": {"title": "for .svg mOperation helper"},
-  "yssPageExample": {"title": "example ( Make your site ) - yss site .svg base data from mqtt"},
-  "three4yss": {"title": "for Three.js sites with .obj .stl .gbl files"},
-  "t4y_ani": {"title": "for Three.js sites to help with animation taskers"},
-  "t4y_console": {"title": "for Three.js sites give console renderer and other ingo use on screen ? key"},
-  "t4y_putText": {"title": "for Three.js sites puts text / OSD"},
-  "t4y_shader": {"title": "for Three.js sites invert / red black / postprocessor"}
+  "mMath": {
+    "title": "mMath and string helper",
+    "file": "./ySS_calibration/libs/mMath.js"},
+  "yssPageExample": {
+    "title": "example ( Make your site ) - yss site .svg base data from mqtt",
+    "file": "./ySS_calibration/sites/basic_sail/s_basicSailPage.js"},
+  "mOperation": {
+    "title": "for .svg mOperation helper",
+    "file": "./ySS_calibration/libs/mOperation.js"},
+  "three4yss": {
+    "title": "for Three.js sites with .obj .stl .gbl files",
+    "file": "./ySS_calibration/libs/three4yss.js"},
+  "t4y_ani": {
+    "title": "for Three.js sites to help with animation taskers",
+    "file": "./ySS_calibration/libs/t4y_ani.js"},
+  "t4y_console": {
+    "title": "for Three.js sites give console renderer and other ingo use on screen ? key",
+    "file": "./ySS_calibration/libs/t4y_console.js"},
+  "t4y_putText": {
+    "title": "for Three.js sites puts text / OSD",
+    "file": "./ySS_calibration/libs/t4y_putText.js"},
+  "t4y_shader": {
+    "title": "for Three.js sites invert / red black / postprocessor",
+    "file": "./ySS_calibration/libs/t4y_shader.js"}
 }'
 
 echo $list | jq '.'
@@ -28,6 +44,7 @@ echo -e "# Index\n\n" > ${otDir}"/yss-js-functions.md"
 
 for js in `echo $list |  jq 'keys[]' -r`; do
   title=`echo $list | jq '.'$js'.title' -r`
+  file=`echo $list | jq '.'$js'.file' -r`
   shUrl=`echo ${title}|sed 's| |-|g'|sed 's|/||g'| sed 's|\.||g' | sed 's|(||g'| \
     sed 's|)||g' | sed "s|'||g" | sed 's|?||g' | sed 's|!||g' `
   echo -e "[- "$title"](#$shUrl)\n\n" >> ${otDir}"/yss-js-functions.md"
@@ -39,16 +56,17 @@ echo -e "\n---\n" >> ${otDir}"/yss-js-functions.md"
 for js in `echo $list |  jq 'keys[]' -r`; do
   echo "js"$js
   title=`echo $list | jq '.'$js'.title' -r`
+  file=`echo $list | jq '.'$js'.file' -r`
   echo "title for file is ... "$title
   echo -e "\n---\n# "${title}"\n### "$js"\n" \
     "[To index](#)" >> ${otDir}"/yss-js-functions.md"
-  echo "running ... npm run jsdoc2md-"$js
-  npm run "jsdoc2md-"$js
+  echo "running ... npm run jsdoc2md-arg" $file
+  npm run jsdoc2md-arg $file
 
 done
 
 echo -e "\n---\n**Powered by**: [jsdoc2md](https://github.com/jsdoc2md)" >> ${otDir}"/yss-js-functions.md"
 
 cp ${otDir}"/yss-js-functions.md" ${wikiDir}"/"
-echo "------exit -------"
-exit 1
+#echo "------exit -------"
+#exit 1
