1c0b745fb8169dc8cfb4003375c936107afced6d
Bartlomiej Ceglik <yoyoek@wp.pl>
2023-02-20 05:50:17 -0500

    pydocTemplate to practice tags what is where in result in document

---
+++ otdm2pydocTemplate.py
@@ -0,0 +1,73 @@
+
+"""
+aaaaaa description of a file
+"""
+class testClass:
+
+	"""
+	bbbbbbb desc of class after init
+	"""
+	def __init__(self, arg1):
+		# : ccccc
+		print("init is ...")
+
+	#: dddddd
+	def abcFunc(self, arg2):
+		return arg2+10
+
+
+#
+# new sectien test
+# can we aad somthing in middle?
+#
+#
+"""
+-----------------
+uuuuuuuuuuuuuuuuuu
+"""
+string666="aaa"
+
+
+def UUEEeeaddItAnd(ax,bx):
+	"""
+	UUEEueueUtility function for loading multiple #Module#s from a list of Python module and package
+	names. It combines #find_module(), #iter_package_files() and #parse_python_module() in a
+	convenient way.
+	# Arguments
+	ax: UUEEueuA list of module names to load and parse.
+	xb: UUEEA list of package names to load and parse.
+	# Returns
+	[Int] - UUEEabccc ooo
+	"""
+	return ax/bx
+
+
+
+
+def addItAnd(a,b):
+	"""
+	Utility function for loading multiple #Module#s from a list of Python module and package
+	names. It combines #find_module(), #iter_package_files() and #parse_python_module() in a
+	convenient way.
+	# Arguments
+	a: A list of module names to load and parse.
+	b: A list of package names to load and parse.
+	# Returns
+	# [Int] - abccc ooo
+	"""
+	return a+b
+
+
+"""
+eeeeeee
+"""
+def abc():
+    """xxxxxx desc of def """
+    # iiiiiii
+    print("Hellooooooooooooooooooooooooooooooooooooooooooooooooooooooo World")
+
+
+# : r r r r
+def addToDocTest1():
+    """ ru ru ru ru **NICE?** """
+    return 10
