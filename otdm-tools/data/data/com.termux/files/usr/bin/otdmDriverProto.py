e7094c33fdf3cb281db36048f647df0a6590ee74
Bartlomiej Ceglik <yoyoek@wp.pl>
2023-02-20 05:27:50 -0500

    otdmDriverProdo xdocs added

--- otdm-tools/data/data/com.termux/files/usr/bin/otdmDriverProto.py
+++ otdm-tools/data/data/com.termux/files/usr/bin/otdmDriverProto.py
@@ -5,8 +5,71 @@ import requests
 import json
 import importlib as il
 
-
 class otdmDriverProto:
+    """
+    This is a programer documentation for otdmDriverProto. If you want to your owne Driver
+    this is a place to be. It's python code explenation of what is in back of the idea and functions.
+    This one is running in shell layer in python3. More about otdmTools info is comming....
+
+    ## Plan in this is
+    To make tasks simple. Making work will be only by saing what to do but how to make it is
+    Handle by selected corect DriverProto. It know if this work is on http API or file system or
+    mysql database using some selections. Not important :)
+    If you have driverProto for it it is in otdm-
+     - [x] You can use your driverProto from yss / bash / otdm- instance
+     - [x] one line tasks any ware
+     - [ ] will do for using in packitso .deb builder
+
+    By implementing your DriverProto you can extend options and join the family otdm-
+
+    ## Example
+    ```bash
+    $ otdmTools.py -dfs '/tmp' -oFile '--'
+    ```
+    *argument dfs if is same as your keyWord your driver will be execute to do the task. `-oFile` store data to output file if -- to stdout.*
+
+    ```bash
+    $ otdmTools.py -dfs '/tmp/abc123' -act MKDIR -oFile '--'
+    ```
+    *`-act` is not GET|POST|DELETE it will try your argument in try So you can put your own function handler and argument vector is pass from otdmTool.py*
+
+    ```bash
+    $ tm=`tempfile`;otdmTools.py -forceHost 192.168.43.220 -dnrfByUid "*" \
+    -oFile ${tm} >> /dev/null
+    cat ${tm} | jq '.[].id'
+    ```
+    *force different host then in .otdm/config.json / use Node-RED driver proto / GETAll () to `tm` file then jq `.id` from result*
+
+    ```bash
+    $ tm=`tempfile`
+    outTemp='{"name":.name,"path":.fullPath}'
+    otdmTools.py -dfs "/tmp" -oFile ${tm} >> /dev/null
+    cat ${tm} | jq '.[] | select( .isFile == false ) | '${outTemp}
+   ```
+   *Returns only directory from /tmp in array of dicts `{"name":.name,"path":.fullPath}`*
+   ```javascript
+   .....
+       "path": "//tmp/jsdoc-api"
+    }
+    {
+      "name": "pyjsdoc",
+      "path": "//tmp/pyjsdoc"
+    }
+   .....
+   ```
+
+    **ver:** 0.2.2
+
+
+    It's for inhereting and overriting some of it's function / method.
+
+    **There is a name restriction** for new files `otdmDriver`YOURNAME`.py`
+    Adding `otdmDriver`YOURNAME`Proto.py` in name will make skip your file in
+    plugin init step.
+
+    All your driversProto should to extand by otdmDriverProto
+    """
+
     deb=False
 
     name=""
@@ -15,10 +78,17 @@ class otdmDriverProto:
     conf=None
 
     keyWord="driverProto"
+    """**keyWord**: string - @overwriteIt important to invoce from arguments from console."""
 
 
 
     def __init__( self, args, conf, name, suffix ):
+        """# Arguments
+        args: dict - with argumens to parse and look for your inputs
+        conf: dict - from otdmTools.py instance .otdm/config.json is comming
+        name: string - to identyfy in .otdm/config.json
+        suffix: string | dict - to pass some args in instance
+        """
         self.name = name
         self.suffix=suffix
         self.args = args
@@ -29,6 +99,7 @@ class otdmDriverProto:
         return self.name
 
     def getHelp( self, implemented=0 ):
+        """@overwriteIt - to set your own help as extra. After the default `$ otdmTools.py -h '1'` """
         if implemented == 0:
             print( f"{self.getName()}.getHelp() Not implemented key word [{self.keyWord}]" )
 
@@ -169,6 +240,9 @@ class otdmDriverProto:
         return 0
 
     def getHost( self ):
+        """@overwriteIt - use rest as curl instance for http api HOST:getPort/getSuffix/getApiPath
+        # Returns
+        [string] - host defined from config and iff set handles -forceHost (then call super)"""
         host = self.conf[ self.getName() ].get("host")
         #print("args:%s"%self.args)
         if self.args.get("forceHost","") != "":
@@ -176,16 +250,26 @@ class otdmDriverProto:
         return host
 
     def getPort( self ):
+        """@overwriteIt - use rest as curl instance for http api getHost:PORT/getSuffix/getApiPath
+        # Returns
+        [string] - port defined from config"""
         return self.conf[ self.getName() ].get("port")
 
     def getSuffix( self ):
+        """@overwriteIt - use rest as curl instance for http api getHost:getPort/suffix/getApiPath
+        # Returns
+        [string] - suffix defined from self.suffix set on super() """
         return self.suffix
 
     def getApiPath( self ):
+        """@overwriteIt - use rest as curl instance for http api getHost:getPort/getSuffix/path
+        # Returns
+        [string] - suffix defined from config"""
         return self.conf[ self.getName() ].get("apiPath")
 
 
     def getHeaders( self,):
+        """@overwriteIt - if want to make http api with this header or overwriteit"""
         return {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
@@ -254,16 +338,29 @@ class otdmDriverProto:
 
 
     def GETAll( self ):
+        """@overwriteIt - to hendle request of all something like all
+        # Returns
+        [list] - of data some form"""
         print("otdmDriverProto.GETAll")
         return self.GET("")
 
     def GET( self, d ):
+        """@overwriteIt - to hendle request of single work read
+        # Arguments
+        d: object - to identyfy your work to get it
+        # Returns
+        [list] - data some form file storable json preffered"""
         print( f"otdmDriverProto.GET({d})" )
         return self.CurlWResChk( "GET",
             f"http://{self.getHost()}:{self.getPort()}{self.getApiPath()}{self.getSuffix()}/{d}"
             )
 
     def POST( self, d ):
+        """@overwriteIt - to hendle request of single work write / make / execute / do taks
+        # Arguments
+        d: object - to put in work as data input to work
+        # Returns
+        [json] - to raport status"""
         print( f"otdmDriverProto.POST({d})" )
         return self.CurlWResChk( "POST",
             f"http://{self.getHost()}:{self.getPort()}{self.getApiPath()}{self.getSuffix()}",
@@ -271,6 +368,9 @@ class otdmDriverProto:
             )
 
     def DELETE( self, d ):
+        """@overwriteIt - to hendle request of deleting / removing / dropping / eresing something like all
+        # Returns
+        [list] - to raport status"""
         print( f"otdmDriverProto.DELETE({d})" )
         sys.exit(1)
 
