f668e8b4484f0db7e83a30c3175e65f9ef698dba
Bartlomiej Ceglik <yoyoek@wp.pl>
2023-02-20 21:39:03 -0500

    rebuild command stacker so pipe "|" can work in pOpen. new errorInStack

--- otdm-tools/data/data/com.termux/files/usr/bin/otdmSubProc.py
+++ otdm-tools/data/data/com.termux/files/usr/bin/otdmSubProc.py
@@ -1,11 +1,11 @@
-from subprocess import Popen, PIPE, STDOUT
-
+import subprocess as sp
 from otdmWcspMqtt import *
 
 class otdmSubProc:
 
     subP = 0
     subOut = []
+    errorInStack = False
     def th_subP( self, args, cb ):
         print(f"sub thread {args['name']} is starting cmd {args['cmd']}")
         print("Popen")
@@ -15,22 +15,75 @@ class otdmSubProc:
         lNo = 0
 
         self.subOut = []
+        self.errorInStack = False
         LastPipe=-1
-        try:
+        if 1:
             print(f"have to check if dont need splitting ... {args['cmd']}")
-            cmdStep=[]
+            cL=[]
+            cmdSets=[]
+            if args['cmd'][0] == "sh":
+                print("doing in sh ......")
+                cmdSets = [args['cmd']]
+                args['cmd'] = []
+
+            print("doing in SubProcesss.Popon ......")
             for a in args['cmd']:
                 if a != "|":
-                    cmdStep.append( a )
-                elif len(cmdStep) > 0:
+                    cL.append( a )
+                elif len(cL) > 0:
+                    cmdSets.append( cL )
+                    cL=[]
+
+            if len(cL) > 0:
+                cmdSets.append( cL )
+                cL=[]
+
+            print(f"so after resections we have cmds .... {cmdSets}")
+
+            for i,s in enumerate(cmdSets):
+                if i == 0:
+                    print("first -- >")
+                    si = sp.PIPE
+                else:
+                    print(" -- > after -- >")
+                    si = self.subP.stdout
+
+                print(f"sub split ...   [{s}] ",end="")
+                try:
+                    self.subP = sp.Popen(
+                        s,
+                        stdout=sp.PIPE, stdin=si,
+                        stderr=sp.STDOUT
+                        )
+                    print("DONE")
+
+                except:
+                    self.errorInStack = True
+                    print(" !! Error !!")
+                    break
+
+                if self.subP.returncode != None:
+                    raise Exception( f'Invalid result: { self.subP.returncode }' )
+
+
+
+            '''
                     print(f"run sub split detected .... on stock")
                     if LastPipe == -1:
-                        print(f"tail run... no before this is first {cmdStep}")
-                        self.subP = Popen(cmdStep,  stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+
+
+
+
+                        print(f"1:  tail run... no before this is first {cmdStep}")
+                        self.subP = sp.Popen(cmdStep,  stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.STDOUT)
+                        print(f"1:    exit code1 ... [{self.subP.returncode}]")
                         LastPipe=1
                     else:
-                        print(f"tail run... next {cmdStep}")
-                        self.subP = Popen(cmdStep,  stdout=PIPE, stdin=self.subP.stdout, stderr=STDOUT)
+                        cmdSets.append( cmdSets )
+                        cmdStep=[]
+                        print(f"2:  tail run... next {cmdStep}")
+                        self.subP = sp.Popen(cmdStep,  stdout=sp.PIPE, stdin=self.subP.stdout, stderr=sp.STDOUT)
+                        print(f"2:    exit code2 ... [{self.subP.returncode}]")
 
                     cmdStep=[]
                 else:
@@ -38,19 +91,29 @@ class otdmSubProc:
 
             if len(cmdStep) > 0:
                 if LastPipe == -1:
-                    print(f"tail run... no before this is first {cmdStep}")
-                    self.subP = Popen(cmdStep,  stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+                    print(f"3:  tail run... no before this is first {cmdStep}")
+                    self.subP = sp.Popen(cmdStep,  stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.STDOUT)
+                    print(f"3;    exit code3 ... [{self.subP.returncode}]")
                 else:
-                    print(f"tail run... next {cmdStep}")
-                    self.subP = Popen(cmdStep,  stdout=PIPE, stdin=self.subP.stdout, stderr=STDOUT)
+                    print(f"4:  tail run... next {cmdStep}")
+                    self.subP = sp.Popen(cmdStep,  stdout=sp.PIPE, stdin=self.subP.stdout, stderr=sp.STDOUT)
+                    print(f"4:    exit code4 ... [{self.subP.returncode}]")
 
-        except:
+            '''
+
+        else:
             print("ojjj Error ")
             self.subOut.append( b"error o.O" )
             self.subOut.append( -1 )
             return 0
 
+        #print("-------------exit")
+        #sys.exit(1)
+
+        print(f"\n------ reader ----------------- any error?[{self.errorInStack}]\n")
+
         cb.subP = self.subP
+        empty=0
         while 1:
             time.sleep(.01)
             lNo+=1
@@ -75,14 +138,13 @@ class otdmSubProc:
                             empty = 0
                 except:
                     print("subOut append read error ")
-
-        print(f"sub thread {args['name']} is DONE")
+                print(f"sub thread {args['name']} is DONE")
 
     def testSubProcAndProm( self, args, cb ):
         print("testSubProcAndProm")
 
         x = threading.Thread( target=self.th_subP, args=({
-            "name": "subprocess name test 1234",
+            "name": "Bash layer",
             "cmd": args
             },cb, ))
         x.start()
@@ -93,14 +155,14 @@ class otdmSubProc:
         while 1:
             nn+= 1
             for l in range( lr, len(self.subOut), 1):
-                if self.subOut[l] != -1:
+                if self.subOut[l] != -1 :
                     li = self.subOut[l]
                     cb.pub( "line", str(f"{l}:{str(li)}") )
                     lr = l+1
 
-            if len(self.subOut)>0 and self.subOut[-1] == -1:
+            if len(self.subOut)>0 and ( self.subOut[-1] == -1 or self.subOut[-1] == -2):
                 break
-            time.sleep(.01)
+            time.sleep(.03)
         #sys.exit(11)
         return 1
 
@@ -128,4 +190,8 @@ class otdmSubProc:
         time.sleep(.5)
 
         #sys.exit(1)
-        return 'SubProcesss DONE'
+        if self.args.get("stdout",'') == '':
+            return 'SubProcesss DONE'
+        else:
+            print("")
+            return self.subOut
