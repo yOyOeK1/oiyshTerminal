99e49e8aaac1026b11c06e46ca68f9fa281bea38
Bartlomiej Ceglik <yoyoek@wp.pl>
2023-02-22 17:03:06 -0500

    notes to how to create tunel to emulator by ssh

--- sshToAndroidWith1880To10880Tunel.sh
+++ sshToAndroidWith1880To10880Tunel.sh
@@ -1,1 +1,9 @@
+
+# /emulator -avd '5.1_WVGA_API_26' -writable-system
+# in termux to make tunel
+# ssh -R 10880:127.0.0.1:1880 192.168.43.1 -p 2222
+# or to me on dell
+# ssh -R 10880:0.0.0.0:1880 yoyo@192.168.43.220 -p 2222
+
+
 ssh -L 10880:localhost:1880 localhost -p 20222
