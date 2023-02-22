dc8877bf9f282b129767b3a5a4aa6c6e7edd7a01
Bartlomiej Ceglik <yoyoek@wp.pl>
2023-02-22 17:00:48 -0500

    otdm-tools-packitso premier otdmPackitsoSh.sh -help

---
+++ otdm-tools-packitso/data/data/com.termux/files/usr/bin/packitsoSh.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+echo -n "- = [ Pack it so - .sh set init ..... "
+
+
+eHelp(){
+  echo 'Help ------
+-help - print this help
+-test - do basic tests
+  '
+}
+
+
+#echo "args count ["$#"]"
+if [ "$#" = "0" ];then
+  echo "No args. Try -help. EXIT 1"
+  exit 1
+else
+  echo "OK ARGS PARSING.."
+
+  case "$1" in
+    "-test" )
+      echo "Arg[-test]"
+      echo "runing test ...."
+      echo "done"
+      exit 0
+    ;;
+    "-help" )
+      echo "Arg[-help]"
+      eHelp
+    ;;
+    * )
+      echo "NaN arg. Try -help. EXIT 1"
+      exit 1
+    ;;
+  esac
+
+
+fi
+
+echo " ] = - DONE"
