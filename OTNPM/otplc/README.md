762724c47ea099c4e8a97ae963707c1719213456
Bartlomiej Ceglik <yoyoek@wp.pl>
2023-03-30 19:39:19 -0500

    otplc for npm

---
+++ OTNPM/otplc/README.md
@@ -0,0 +1,103 @@
+## oiyshTerminal - plc
+
+   Is oiyshTerminal solution for chaos in pins layers. We have many pins different types ins/outs in our systems. Sometimes one mcu is making something in more then one device (that is in my case). Adding PLC layer virtual we can organize all that is some sort of order.
+
+
+
+This library is to help with managing otplc's.
+
+
+
+## most basic layer otplc
+
+Every thing have it's group. I call it `*plcType` for me it's battey, led, swith, button, relay, mic, temperature, rpm, .... We can put label of `*name` to every thing. Also `location` is important for later to know where it's. If it's a locker ok a car is in car. Over what type of protocol is mainly use `srcName` is like http, tcp, ws, mqtt, fs, ....  protocol over what we got it. And last in more detail `srcD` address. I case of mqtt: topic | ws: url | fs: path | ....    
+
+*** need to set**
+
+
+
+### making instance : new
+
+```javascript
+let o = require("./otplc");
+var otplc = o.otplc;
+```
+
+To have a option to play with it ....
+
+
+
+### adding new : .add
+
+To add new battery for example you can make it as example. It is a need way to sort things. All battery this way cane be treated similar. And you know how many devices there is in one location
+
+```javascript
+otplc.add(
+    'battery',        // as plcType
+    'Home LiPo4',     // as name
+    'oiysh',          // as location
+    'mqtt',           // srcName
+    'e01Mux/adc0',    // srcD
+    -1                // extra data for this one from you to store
+    );
+```
+
+**Return** {string} : your name of ot-plc
+
+
+
+### get it all : .getAll
+
+Is stored in `const` so it should be same for all but if you want to marge different instances invoke ....
+
+**Return** {array} : of ot-plc's
+
+```javascript
+[{
+    "plcType":"dummy",
+    "name":"abc",
+    "srcName":"",
+    "srcD":"",
+    "extra":-1
+},{ ..... }]
+```
+
+
+
+
+
+
+
+
+
+## runing test1
+
+To have a way to see what is going on
+
+```javascript
+let o = require("./otplc");
+let t = new o.OTplcTest1();
+> OTplcTest1 .... 
+> go run test 1 ...... Thu Mar 30 2023 11:27:51 ....
+> OTplc init ...
+> will have sufix .... mic_0
+> Total in: 5
+> Types: battery, led
+> ---------------------
+> All typel srcName
+> [ 'mqtt', '' ]
+> test 1 ........... DONE Exit 0
+> in op is the instance to play ...
+let o = t1.op;
+// so o is our structure of otPlcs
+```
+
+
+
+  
+
+## status
+
+## screenshots
+
+## notes
---
+++ OTNPM/otplc/index.js
@@ -0,0 +1,136 @@
+
+function cl( m ){
+  console.log( m );
+}
+
+
+class OTplc{
+
+  constructor(){
+    cl("OTplc init ...");
+    this.plcs = [];
+    this.plcsCount = 0;
+  }
+
+  isIn( name ){
+    return this.plcs.find((e,i)=>{ if(e.name == name) return true; }) ? true : false;
+  }
+
+  zeroAll(){
+    this.plcs = [];
+    return true;
+  }
+
+
+  add(  plcType, name, location='', srcName='', srcD='', extra = -1){
+    let suf = '';
+    if( this.isIn( name ) == true ){
+      for(let s=0,sc=1024; s<sc; s++ ){
+        suf = `_${s}`;
+        if( this.isIn( name+suf ) == false )
+          break;
+      }
+      cl("will have sufix .... "+name+suf);
+    }
+
+    this.plcs.push({
+      'plcType': plcType,
+      'name': name+suf, 'location': location,
+      'srcName': srcName, 'srcD': srcD,
+      'extra': extra
+    });
+    this.plcsCount++;
+
+    return name+suf;
+  }
+
+  getAll(){
+    return this.plcs;
+  }
+
+  getAllTypes(lType){
+    let trt = {};
+    let tr = [];
+    for(let p=0; p<this.plcsCount; p++ ){
+      cl(this.plcs[ p ]);
+      if( trt[ this.plcs[ p ][ lType ] ] == undefined ){
+          trt[ this.plcs[ p ][ lType ] ] = 1;
+          tr.push( this.plcs[ p ][ lType ] );
+      }
+    }
+    return tr;
+  }
+
+  get( fillterBy = {} ){}
+
+}
+
+class OTplcTest1 {
+  constructor() {
+    cl('OTplcTest1 .... ');
+
+    cl("go run test 1 ...... "+(new Date()))
+
+    let op = new OTplc();
+
+    op.add('battery', 'dell',             'isDell', 'mqtt', 'devSen/isDell/bat/perc' );
+    op.add('led',     'mic',              'Hu'  );
+    op.add('battery', 'Home LiPo4',       'oiysh', 'mqtt', 'e01Mux/adc0' );
+    op.add('battery', 'Tesla 24 -> 12v',  'oiysh', 'mqtt', 'e01Mux/adc2'  );
+
+    op.add('led',     'mic',              'isDell'  );
+
+    let pAll = op.getAll();
+    let pTypes = op.getAllTypes('plcType');
+
+    cl(`Total in: ${pAll.length}
+Types: ${pTypes.join(', ')}
+---------------------`);
+    cl("All typel srcName");
+    cl( op.getAllTypes( 'srcName' ) );
+
+
+    cl("test 1 ........... DONE Exit 0");
+
+    this.op = op;
+    cl("in op is the instance to play ...");
+
+  }
+}
+
+const otplc = new OTplc();
+
+try{
+  module.exports = {
+    OTplc,
+    OTplcTest1,
+    otplc,
+  };
+}catch(e){
+  cl("Is not in module mode ...");
+}
+
+
+var args = {};
+process.argv.forEach(function (val, index, array) {
+  //console.log(index + ': ' + val);
+  if( index >= 2 ){
+    let s = val.split('=');
+    args[ s[0] ] = s[1] == undefined ? 1 : s[1];
+  }
+});
+
+if( args != {} ){
+  cl("args to parse :");
+  cl(args);
+
+
+  if( args['runTestErr'] == 1 ){
+    cl("go Exit with 1")
+    process.exit(1);
+  } else if( args['runTest1'] == 1 ){
+    new OTplcTest1();
+  }
+
+
+}
