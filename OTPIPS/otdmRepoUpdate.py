bec151f68d794bc61ef42a47f19290c038bd5b51
Bartlomiej Ceglik <yoyoek@wp.pl>
2023-05-12 12:47:10 -0500

    otdmRepoUpdate.py and REPOPIPS/ot_my_libs is landed

--- OTPIPS/otdmRepoUpdate.py
+++ OTPIPS/otdmRepoUpdate.py
@@ -1,10 +1,13 @@
 #! /usr/bin/python3.8
 
 from ot_my_libs.FileActions import FileActions as fad
+from ot_my_libs.TimeHelper import TimeHelper as thd
 import json,sys,os
 
+REPO_DIR = "./REPOPIPS"
 
 fa = fad()
+th = thd()
 
 def getJson():
 	global fa
@@ -20,13 +23,31 @@ def getJson():
 
 def copyIt( srcUrl ):
 	global fa
-	cmd= f"cp {srcUrl} ./REPOPIPS/"
-	print("		copyIt ... ",end="")
+	global REPO_DIR
+	cmd= f"cp {srcUrl} {REPO_DIR}/"
+	print(f"		copyIt ... to {REPO_DIR}	",end="")
 	#print(cmd)
 	#fa.cp( srcUrl,
 	os.popen(cmd)
-	print("DONE")
+	print("  DONE")
 	
+def repoGlobalListUpdate():
+	global fa
+	global REPO_DIR
+	global th
+	files = []
+	for fileT in fa.getFileList(REPO_DIR):
+		fs = fileT.split('.')
+		if fs[-1] in ["whl","gz"]:
+			files.append( fileT )
+			
+	print("have list ....")
+	print(files)
+	fa.writeFile( f"{REPO_DIR}/pips.json", json.dumps({
+		"files":files,
+		"entryDate": th.getTimestamp(),
+		"last update": th.getNiceDateFromTimestamp()
+	}, indent=2) )	
 
 def repoUpdate():
 	print( "Pips repository update ..." )
@@ -43,6 +64,11 @@ def repoUpdate():
 		path = f"{packKey}/dist/{packKey}-{ver}*"
 		print(f"	[ {i+1} / {totalC} ] {packKey}	....	[{path}]")
 		copyIt( path )
+		
+	print("- making new repo global list ...", end="")
+	repoGlobalListUpdate()
+	print("  DONE")
+	
 	#names="$( cat ./Repo.json | jq '.[].name' -r )"
 #names="$( cat ./Repo.json | jq '.[].name' -r )"
 
