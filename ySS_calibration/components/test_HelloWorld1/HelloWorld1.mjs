902c149c12b8fecd1a28acdc94fb5f6564033aa3
Bartlomiej Ceglik <yoyoek@wp.pl>
2023-05-17 22:11:24 -0500

    components/test_HelloWorld1 not there yet !

---
+++ ySS_calibration/components/test_HelloWorld1/HelloWorld1.mjs
@@ -0,0 +1,16 @@
+
+
+
+function hcl( m ) {
+	console.log("test1_HH : ",m);
+}
+
+hcl("test1_HelloWorld1 init ...");
+
+import { createApp } from 'vue'
+
+//import HelloWorld from './HelloWorld1.vue'
+export function ymHelloWorld1() {
+	hcl(" function call ....");
+	
+}
---
+++ ySS_calibration/components/test_HelloWorld1/HelloWorld1.vue
@@ -0,0 +1,8 @@
+<script>
+console.log("hello world 1 from .vue file ");
+
+</script>
+
+<template>
+	<div>test1 hello world</div>
+</template>
