4eb4b80ebe4646c73b744a706dfd92c3ee12a4be
Bartlomiej Ceglik <yoyoek@wp.pl>
2023-05-19 09:03:30 -0500

    some tweaks in logs more ..

--- OTPIPS/otdmUpload2Pip.sh
+++ OTPIPS/otdmUpload2Pip.sh
@@ -5,24 +5,26 @@ puPath="$p"
 
 echo -n "* file looks ok ... "
 if [ -f "$puPath"".tar.gz" ]; then
-    echo "OK"
 else
     echo ""
     echo "Error wrong path? no file at ""$puPath"".tar.gz EXIT 11"
     exit 11
 fi
+echo "OK"
 
 
-echo "- setting up proxy ... "
+echo -n "* setting up proxy ... "
 mP="http://192.168.43.1:8118"
 export HTTP_PROXY="$mP"
 export HTTPS_PROXY="$mP"
 export ALL_PROXY="$mP"
-
+echo "OK"
 
 #echo "$puPath* exit 3 for now "
 #exit 3
 
-echo "- twine upload ...."
+echo -n "* twine upload ... "
 python3 -m twine upload --repository testpypi "$puPath*"
+echo "OK"
+
 echo "DONE
